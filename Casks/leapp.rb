5c4450c3d05db783f8713681ea47a6b2f24bbc85
Andrea Cavagna <a.cava94@gmail.com>
2021-11-17 11:01:33 +0100

    updated Leapp to version v.0.7.4

--- Casks/leapp.rb
+++ Casks/leapp.rb
@@ -1,5 +1,5 @@
 cask "leapp" do
-  version "0.7.3"
+  version "0.7.4"
   sha256 "aa17132a6efcdadbc45e7e3faf990c3ac91700926c79d164a80ff65225c2bb69"
 
   url "https://asset.noovolari.com/#{version}/Leapp-#{version}.dmg",
